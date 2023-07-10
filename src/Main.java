1dbd12223961d7f5849e21767bd4ebf6574dd01c
misterabvir <abvir@yandex.ru>
2023-07-10 09:19:05 +0300

    Рефакторинг

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,11 @@
 package src;
 
-
+import src.views.ConsoleView;
+import src.views.View;
 
 public class Main {
     public static void main(String[] args) {
-        
+        View view = new ConsoleView();
+        view.start();
     }
 }
